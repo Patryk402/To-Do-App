90ee46568b4dea376156d00a410f5b7842ef7956
Patryk <patryk402@op.pl>
2021-11-08 15:21:03 +0100

    updates

--- src/component/AddWish.js
+++ src/component/AddWish.js
@@ -1,32 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const AddWish = (props) => { // komponent funkcyjny tworzący bazę zmapowanych elementów i tworzy strkturę komponentu wyświetlanego w porzednim, wywołujący funkcje
-    const style = { color: "red" } // style do wartośc important czyli elementu na czerwono
-    const day = new Date(props.day).toLocaleString()// konwetowanie obiektu date na datę i godzinę 
-    if (props.active) { // jeżeli active jest true zwróć tą zawartość (życzenia do zrobienia)
-        return (
-            <>
-            <li>
-                {props.important? <strong style={style}>{props.name} </strong> : <strong>{props.name}</strong>} {/* instrukcja zmieniająca kolor tekstu w zależności od statusu checkboxu inportant */}
-                <em> zrobić do </em>
-                <span> {props.data} </span> {/* wyświetlanie daty utworzenia życzenia */}
-                <button onClick={() => props.change(props.id)}>Zrobione</button> {/* na podstawie id z obiekty state w App.js wywoływana jest funkcja zmieniająca active na false klikniętego elementu */}
-                <button onClick={() => props.delete(props.id)}>X</button> {/* funkcja filtrująca która usuwa kliknięty element z listy */}
-            </li>
-                <br />
-                </>
-             )
-        }
-    else {// jeżeli active  jest false zwróć to (zrobione życzenia)
-        return (
-            <li>
-                <strong>{props.name}</strong>
-                <em> ( zrobione dnia {day} ) </em> {/* Wyświetlanie daty i godziny zrobionego życzenia*/}
-                <button onClick={() => props.delete(props.id)}>X</button>
-             </li>
-             )
-      }
-    
-}
- 
+const AddWish = (props) => {
+	const style = { color: "red" };
+	const day = new Date(props.day).toLocaleString();
+	if (props.active) {
+		return (
+			<>
+				<li>
+					{props.important ? (
+						<strong style={style}>{props.name} </strong>
+					) : (
+						<strong>{props.name}</strong>
+					)}
+					<em> make to </em>
+					<span> {props.data} </span>
+					<button onClick={() => props.change(props.id)}>DONE</button>
+					<button onClick={() => props.delete(props.id)}>X</button>
+				</li>
+				<br />
+			</>
+		);
+	} else {
+		// jeżeli active  jest false zwróć to (zrobione życzenia)
+		return (
+			<li>
+				<strong>{props.name}</strong>
+				<em> ( done on day {day} ) </em>
+				<button onClick={() => props.delete(props.id)}>X</button>
+			</li>
+		);
+	}
+};
+
 export default AddWish;
--- src/component/NewWish.js
+++ src/component/NewWish.js
@@ -1,71 +1,82 @@
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
+class NewWish extends Component {
+	state = {
+		text: "",
+		date: new Date().toISOString().slice(0, 10),
+		checked: false,
+	};
+	handleText = (e) => {
+		const text = e.target.value;
+		this.setState({
+			text,
+		});
+	};
+	handleChecked = (e) => {
+		const checked = e.target.checked;
+		this.setState({
+			checked,
+		});
+	};
+	handleDate = (e) => {
+		const date = e.target.value;
+		this.setState({
+			date,
+		});
+	};
+	handleSend = (e) => {
+		e.preventDefault();
+		const { text, date, checked } = this.state;
+		const add = this.props.add(text, date, checked);
+		if (add) {
+			this.setState({
+				text: "",
+				date: new Date().toISOString().slice(0, 10),
+				checked: false,
+			});
+		}
+	};
 
-class NewWish extends Component { // komponent przechowujący wszystkie dane i funkcje służące do stworzenia nowego obiektu do tablicy w obiekcie state komponentu App.js
-   
-    state = { // przechowuje zawartości inputa text, checked inputa checkbox i datę uciętą metodą slice() która po ucięciu pokazuje DD-MM-YY
-        text: "",
-        date: new Date().toISOString().slice(0,10),
-        checked: false,
-    }
-    handleText = (e) => { // funkcja onChange zmieniająca zawartośc obiektu statne na value z inputa text
-        const text = e.target.value
-        this.setState(
-            {
-            text
-            }
-        );
-    }
-    handleChecked = (e) => { // funkcja onChange zmieniająca zawartośc obiektu statne na checked z inputa checkbox
-        const checked = e.target.checked
-        this.setState(
-            {
-                checked
-            }
-        );
-    }
-    handleDate = (e) => { // funkcja onChange zmieniająca zawartośc obiektu statne na value z inputa date
-        const date = e.target.value
-        this.setState(
-            {
-            date
-            }
-        );
-    }
-    handleSend = (e) => { // funkcja przekazywana jako props do komponentu App.js wywołująca dodanie nowego obiektu do state w App.js
-        e.preventDefault()
-        const { text , date , checked} = this.state // desktukturyzacja 
-        const add = this.props.add(text , date , checked)  // przekazywanie argumentów destrukturyzacji propsem do komponentu App.js(w nim jest jako add który wywołuje tamtejszą funkcje handle add i przekazuje jej parametry stąd)
-        if (add) { 
-            this.setState({ // czyszczenie pól w obiekcie state po kliknięciu jeżeli funkcja handleAdd zwraca true
-                text: "",
-                date: new Date().toISOString().slice(0, 10),
-                checked: false
-            })
-        }
-    }
-    
-    render() {
-        const minDate = new Date().toISOString().slice(0, 10) // tworzeie minimalnej daty do inputa date
-        let maxDate = minDate.slice(0,4) * 1 + 1 // tworzenie maksmalnej daty do inputa date tworząc rok
-        maxDate = maxDate + "-12-29" // łączenie maksymalnej daty w całość dodając miesiać i dzień 
-        return (
-            <>
-                <form onSubmit = {this.handleSend}> {/* formularz zawierający inputy uzupełniające obiekt state i przycisk dodający nowy obiekt(nowe życzenie)*/}
-                <h2> Add New Wish </h2>
-                <input style = {{fontSize:20, padding:10}} type="text" onChange = {this.handleText} placeholder="dodaj życzenie" value={this.state.text} /> <input type="checkbox" onChange = {this.handleChecked}  checked={this.state.checked}/> <strong> priorytet</strong>
-                <br />
-                <br />
-                <strong>do kiedy</strong> <input type="date" onChange = {this.handleDate}  value = {this.state.date} min = {minDate} max = {maxDate} />
-                <br />
-                <br />
-                <button style = {{ fontSize: 20 , color: "blue" , padding: 10 }}>Add Wish</button>
-                </form>
-                
-            </>    
-          );
-    }
-  
+	render() {
+		const minDate = new Date().toISOString().slice(0, 10);
+		let maxDate = minDate.slice(0, 4) * 1 + 1;
+		maxDate = maxDate + "-12-29";
+		return (
+			<>
+				<form onSubmit={this.handleSend}>
+					<h2> Add New Wish </h2>
+					<input
+						style={{ fontSize: 20, padding: 10 }}
+						type="text"
+						onChange={this.handleText}
+						placeholder="write wish..."
+						value={this.state.text}
+					/>{" "}
+					<input
+						type="checkbox"
+						onChange={this.handleChecked}
+						checked={this.state.checked}
+					/>{" "}
+					<strong> important</strong>
+					<br />
+					<br />
+					<strong>release date</strong>{" "}
+					<input
+						type="date"
+						onChange={this.handleDate}
+						value={this.state.date}
+						min={minDate}
+						max={maxDate}
+					/>
+					<br />
+					<br />
+					<button style={{ fontSize: 20, color: "blue", padding: 10 }}>
+						Add Wish
+					</button>
+				</form>
+			</>
+		);
+	}
 }
- 
+
 export default NewWish;
